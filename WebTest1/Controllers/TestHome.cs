f180c29cdad8c10dffae9d8483d48b14221fa404
VinceGonzales <vince.gonzales@hotmail.com>
2018-10-15 09:03:22 -0700

    unit test RouteConfig

--- WebTest1/Controllers/TestHome.cs
+++ WebTest1/Controllers/TestHome.cs
@@ -4,21 +4,36 @@ using WebApplication1.Models;
 using WebApplication1.Controllers;
 using Moq;
 using System.Web.Mvc;
+using System.Web.Routing;
+using WebApplication1;
 
 namespace WebTest1.Controllers
 {
     [TestClass]
     public class TestHome
     {
+        private HomeController _controller;
+        private Mock<IRepository> mockRepository;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            mockRepository = new Mock<IRepository>();
+            _controller = new HomeController(mockRepository.Object);
+        }
+
         [TestMethod]
         public void Home_Should_Display_Index_Page()
         {
             // Arrange
-            Mock<IRepository> mockRepo = new Mock<IRepository>();
-            HomeController controller = new HomeController(mockRepo.Object);
+            RouteTable.Routes.Clear();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            var httpContext = MvcMockHelpers.MockHttpContext(@"~/");
+            var routeData = RouteTable.Routes.GetRouteData(httpContext);
+            _controller.SetMockControllerContext(httpContext, routeData, RouteTable.Routes);
 
             // Act
-            ViewResult result = controller.Index() as ViewResult;
+            ViewResult result = _controller.Index() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
